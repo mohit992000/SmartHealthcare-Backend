56e2b3cd8927b95ded23a9fbb352d78e2b377e94
Mohit Kumar <42437153+mohit992000@users.noreply.github.com>
2025-02-27 18:50:50 -0500

    Updated authentication and protected routes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const morgan = require("morgan");
 const { verifyToken, authorizeRoles } = require("./authMiddleware");// Import middleware
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("combined")); // Enable request logging
+
 
 // MySQL Database Connection
 const db = mysql.createConnection({
